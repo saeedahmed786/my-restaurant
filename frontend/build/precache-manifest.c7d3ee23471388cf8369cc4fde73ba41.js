a4ab88dbf2ef86e6f5472226373f1c96269bcb28
Saeed Ahmed <saeedchachar987654@gmail.com>
2020-10-22 06:41:41 -0700

    new change

---
+++ frontend/build/precache-manifest.c7d3ee23471388cf8369cc4fde73ba41.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b279375c6872d820fa81c6b91061c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd55db8b91b786cbde32",
+    "url": "/static/css/main.6162959c.chunk.css"
+  },
+  {
+    "revision": "22681bb066137f6ed56f",
+    "url": "/static/js/2.775ef49b.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.775ef49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd55db8b91b786cbde32",
+    "url": "/static/js/main.22e6ecd8.chunk.js"
+  },
+  {
+    "revision": "6b52c196c61487d2052b",
+    "url": "/static/js/runtime-main.d99813ef.js"
+  },
+  {
+    "revision": "3ab24fe8468144a4d29fadf1fb00def8",
+    "url": "/static/media/bg-image.3ab24fe8.jpg"
+  },
+  {
+    "revision": "2848091b03b3cdc3c6b81df3d7886a1e",
+    "url": "/static/media/cansara.2848091b.jpg"
+  },
+  {
+    "revision": "6f4661fb6524cd1942b63ee65a1a658a",
+    "url": "/static/media/header-image.6f4661fb.jpg"
+  },
+  {
+    "revision": "f82194975bc7c6253a0c669f1dfa78ca",
+    "url": "/static/media/iphone.f8219497.jpg"
+  },
+  {
+    "revision": "e752cf43a7c71c93f6d195a7234a6145",
+    "url": "/static/media/logo.e752cf43.png"
+  }
+]);
